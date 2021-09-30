fe007582aaa45eaa2a0a534c531d8ac6bb9d5178
Hector <yoga2005live@gmail.com>
2021-09-30 18:59:33 -0500

    all commands in the same place

--- cdk/lib/CdkPipeLineStack.js
+++ cdk/lib/CdkPipeLineStack.js
@@ -1,6 +1,7 @@
 const cdk = require('@aws-cdk/core');
 const {CodePipeline, CodePipelineSource, ShellStep} = require('@aws-cdk/pipelines');
 const s3 = require('@aws-cdk/aws-s3')
+const s3Deploy = require('@aws-cdk/aws-s3-deployment')
 const {SecretValue} = require("@aws-cdk/core");
 
 class CdkPipeLineStack extends cdk.Stack {
@@ -13,27 +14,6 @@ class CdkPipeLineStack extends cdk.Stack {
     constructor(scope, id, props) {
         super(scope, id, props);
 
-        const s3Staging = new s3.Bucket(this, 'staging', {
-            removalPolicy: cdk.RemovalPolicy.DESTROY,
-        });
-
-//Provando
-        const repoSource = CodePipelineSource.gitHub('yoga2005live/reactapp', 'master',
-            {authentication: SecretValue.secretsManager('arn:aws:secretsmanager:us-east-2:975663573741:secret:github-oauth-token_1-80vZpc')}
-        )
-
-        const reactBuildApp = new ShellStep('reactBuildApp', {
-            input: repoSource,
-            primaryOutputDirectory: './build',
-            commands: [
-                'ls -a -l --color',
-                'npm i',
-                'npm run build',
-                'ls -a -l --color',
-            ]
-        });
-//Provando
-
         const pipeline = new CodePipeline(this, 'reactAppPipelineId', {
             pipelineName: 'reactAppPipelineName',
             synth: new ShellStep('Synth', {
@@ -41,6 +21,11 @@ class CdkPipeLineStack extends cdk.Stack {
                     {authentication: SecretValue.secretsManager('arn:aws:secretsmanager:us-east-2:975663573741:secret:github-oauth-token_1-80vZpc')}
                 ),
                 commands: [
+                    'npm i',
+                    'ls -a -l --color',
+                    'npm run build',
+                    'ls -a -l --color',
+                    // 'mv build reactApp',
                     'ls -a -l --color',
                     'cd cdk', 'npm ci',
                     'ls -a -l --color',
@@ -53,6 +38,32 @@ class CdkPipeLineStack extends cdk.Stack {
                 // output: s3Staging
             })
         });
+
+//Provando
+        const s3Staging = new s3.Bucket(this, 'staging', {
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+        });
+
+
+//         const repoSource = CodePipelineSource.gitHub('yoga2005live/reactapp', 'master',
+//             {authentication: SecretValue.secretsManager('arn:aws:secretsmanager:us-east-2:975663573741:secret:github-oauth-token_1-80vZpc')}
+//         )
+//
+//         const reactBuildApp = new ShellStep('reactBuildApp', {
+//             input: repoSource,
+//             primaryOutputDirectory: './build',
+//             commands: [
+//                 'ls -a -l --color',
+//                 'npm i',
+//                 'npm run build',
+//                 'ls -a -l --color',
+//             ]
+//         });
+        const s3Deployment = new s3Deploy.BucketDeployment(this, 'cdkDeployBucket', {
+            sources:[s3Deploy.Source.asset("../build")],
+            destinationBucket: s3Staging
+        });
+//Provando
 
     }
 }
